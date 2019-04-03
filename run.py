6e7b041b6ecfa74e951d5929a71b717f32e13eaa
Octaviotastico <octaviopercivaldi2@gmail.com>
2019-04-02 21:29:49 -0300

    Added performance function to run.py

--- run.py
+++ run.py
@@ -77,7 +77,73 @@ def penazzi(graph, mode):
     outfile.close()
 
 def performance(graph, mode):
+    # Command for performance test
+    command = ['make performance', 'SWITCH=', 'RMBC=', 'INPUT=', 'OUTPUT=', 'OFLAG=']
 
+    # Arguments for command
+    sw, rm, inp, out, ans = None, None, 'samples/performance/' + graph, 'out/performance/' + graph, 'ans/performance/' + graph
+
+    # Important files
+    ansfile, outfile = None, None
+
+    # Check that inputfile exits
+    if not os.path.isfile(inp):
+        print("Graph " + graph + " does not exist")
+        exit(0)
+    
+    # We open ansfile
+    try:
+        ansfile = open(ans, 'r')
+    except OSError:
+        print("Answer file for " + graph + " not found.")
+        exit(0)
+
+    # Read SWITCH and RMBC
+    try:
+        sw, rm = ansfile.readline().split()
+    except ValueError:
+        print("Answer file has wrong format, 'sw rm' needed")
+        exit(0)
+
+    # We open outfile, creating it if necessary
+    try:
+        outfile = open(out, 'r')
+    except OSError:
+        if not os.path.isdir('out'):
+            os.mkdir('out')
+        os.mknod(out)
+
+    args = [sw, rm, inp, out]
+
+    for i in range(1, len(command) - 1):
+        command[i] += args[i - 1]
+
+    if(mode):
+        command[-1] += '-DHARD'
+
+    command = ' '.join(command)
+
+    os.system(command)
+
+    message = 'Runned performance test with ' + sw + ' switches and ' + rm + ' RMBCs'
+
+    print(message)
+
+    print('Time spent creating graph: ' + outfile.readline())
+    next(outfile)
+    print('Time spent running Natural: ' + outfile.readline())
+    next(outfile)
+    print('Time spent running Welsh: ' + outfile.readline())
+    next(outfile)
+    print('Time spent running Switch: ' + outfile.readline())
+    next(outfile)
+    print('Time spent running RMBC: ' + outfile.readline())
+    print('Time spent destroying graph: ' + outfile.readline())
+    print('Penazzi time: ' + ansfile.readline())
+    print('Your time: ' + outfile.readline())
+
+    ansfile.close()
+    outfile.close()
 
 
 
@@ -175,7 +241,7 @@ def main():
     if test == 'penazzi':
         penazzi(graph, options.diff)        
     elif test == 'performance':
-        pass
+        performance(graph, options.diff)
     elif test == 'memory':
         pass
     else:
